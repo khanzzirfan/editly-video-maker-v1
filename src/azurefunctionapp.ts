80a3d57f3de36baa4b2fc0cc1b83e0376c5749af
Irfan Khan <irfan@infopower.co.nz>
2023-11-23 00:12:37 +1100

    fix: updated packages

--- src/azurefunctionapp.ts
+++ src/azurefunctionapp.ts
@@ -1,24 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import serverlessExpress from '@vendia/serverless-express';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import express from 'express';
 import { AppModule } from './app.module';
 
 let cachedServer: any;
-
-/** Enable swagger */
-function setupSwaggerDocument(app) {
-  const config = new DocumentBuilder()
-    .setTitle('editly-video-maker')
-    .setDescription('editly-video-maker')
-    .setVersion('1.0')
-    .addTag('editly-video-maker')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('/api/swagger-ui', app, document);
-}
-
 async function bootstrap() {
   if (!cachedServer) {
     const expressApp = express();
@@ -29,7 +15,6 @@ async function bootstrap() {
     nestApp.enableCors();
     await nestApp.init();
     // enable swagger
-    setupSwaggerDocument(nestApp);
     cachedServer = serverlessExpress({ app: expressApp });
   }
   return cachedServer;
